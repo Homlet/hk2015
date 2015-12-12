4273fac8b8ab8b41be62ac62c296f120c02f43bf
Hgat <hgat@hotmail.co.uk>
2015-12-12 20:49:20 +0000

    Fixes for stuff

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var irc = require('irc');
+var messagebird = require('messagebird')('live_c824ASBElBQE46yxKwzeK3e4a');
 
 var clients = [];
 
-var addClient = function(nick, cb) {
-  var client = new irc.Client(server, nick, chans);
+var addClient = function(server, nick, chans, cb) {
+  var client = new irc.Client(server, nick, {
+    channels: chans
+  });
 
   client.addListener('error', function(message) {
     console.log('error: ', message);
@@ -11,10 +14,27 @@ var addClient = function(nick, cb) {
 
   client.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
-    //TODO: push to sms
+    var params = {
+      'originator': 'Hackkings',
+      'recipients': [
+        '+4407895331096'
+      ],
+      'body': from + ': ' + message
+    };
+
+    messagebird.messages.create(params, function (err, response) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(response);
+    });
   });
 
   clients.push(client);
 
   cb(clients.length - 1);
 };
+
+addClient('chat.freenode.net', 'hack156672', ['#hackkings'], function(id) {
+
+});
